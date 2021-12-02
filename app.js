452555f92d11ba84aa5031d7803e39e66d6db2d0
Auddity <audsecondarylap@outlook.com>
2021-12-02 18:21:41 -0500

    map marker

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([42.097, -79.23], 13);
+var marker = L.marker([42.096, -79.236]).addTo(map);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
